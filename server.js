31a4376ba1a63d14c5bc3a8315f3c2084a4b88d3
Fernando Trouw <ftrouw@protonmail.com>
2021-12-28 14:33:27 +0700

    procfile to beginning

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+var host = process.env.HOST || 'localhost';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
